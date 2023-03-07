f2ba203e326638a7681670462e997c2a58aecbdf
S. Sarantsev <sarantsev.s@gmail.com>
2023-03-07 11:59:57 +0100

    enable eq env map

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,13 +1,16 @@
 import {useFrame, useThree} from "@react-three/fiber";
-import {OrbitControls} from "@react-three/drei";
+import {OrbitControls } from "@react-three/drei";
 import React, {useEffect, useRef} from "react";
-import { Vector3 } from "three"
+import {Vector3} from "three"
 import {OrbitControls as OrbitControlsType} from "three-stdlib";
 import SolarSystem from "./SolarSystem";
+import useEquirectangularMap from "../hooks/useEquirectangularMap";
 
 const Scene: React.FC = () => {
     const { camera } = useThree()
     const controls = useRef<OrbitControlsType>(null)
+
+    const { cube } = useEquirectangularMap({ path: 'textures/solarsystemscope.com/8k_stars_milky_way.jpg'})
 
     useEffect(() => {
         camera.position.set(10, 10, 10)
@@ -28,6 +31,7 @@ const Scene: React.FC = () => {
 
     return (
         <>
+            {cube && <primitive attach='background' object={cube.texture} />}
             <ambientLight />
             <pointLight position={[1000, 1000, 1000]} />
             <gridHelper
